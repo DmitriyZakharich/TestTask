ee5f7d550975035eb7c9e4fa2ce45b810d67de15
DmitriyZakharich <zakharichd@yandex.ru>
2023-12-20 19:43:03 +0300

    Refactoring

--- app/src/main/java/com/example/testtasktutu/screens/details_screen/domain/interfaces/GetDataUseCase.kt
+++ app/src/main/java/com/example/testtasktutu/screens/details_screen/domain/interfaces/GetDataUseCase.kt
@@ -1,9 +1,7 @@
 package com.example.testtasktutu.screens.details_screen.domain.interfaces
 
-import androidx.lifecycle.LiveData
 import com.example.testtasktutu.screens.details_screen.domain.model.GithubDetailRepoInfoDomain
 
 interface GetDataUseCase {
-//    val info: LiveData<GithubDetailRepoInfoDomain>
-suspend fun start(login: String, name: String): GithubDetailRepoInfoDomain
+    suspend fun start(login: String, name: String): GithubDetailRepoInfoDomain
 }
--- app/src/main/java/com/example/testtasktutu/screens/list_screen/domain/models/ParcelGithubRepoBriefInfo.kt
+++
@@ -1,6 +0,0 @@
-package com.example.testtasktutu.screens.list_screen.domain.models
-
-data class ParcelGithubRepoBriefInfo(
-        val isSuccess: Boolean,
-        val list: List<GithubRepoBriefInfoDomain>?
-)
